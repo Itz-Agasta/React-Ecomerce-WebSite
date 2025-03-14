cc36ac8ec4a2fccb6775ab18ad286b9e1fb4f7b6
Itz-Agasta <rupamgolui69@gmail.com>
2025-03-14 21:58:28 +0530

    Configure Vite server to listen on all addresses and set port to 5173

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { ViteAliases } from 'vite-aliases'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { ViteAliases } from "vite-aliases";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  ViteAliases()],
-})
+  plugins: [react(), ViteAliases()],
+  server: {
+    host: true, // Listen on all addresses
+    port: 5173,
+  },
+});
